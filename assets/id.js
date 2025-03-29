bd06df6e09edb92c5c106bff92fd1dd8a7e7230c
Przemek124l <100531144+WdsSsss@users.noreply.github.com>
2025-03-29 22:32:16 +0100

    id.js

--- assets/id.js
+++ assets/id.js
@@ -57,16 +57,4 @@ function delay(time, length) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-    var classlist = eye.classList;
-    if (classlist.contains("eye_close")){
-        classlist.remove("eye_close");
-        var dots = "";
-        for (var i = 0; i < input.value.length - 1; i++){
-            dots = dots + dot
-        }
-        input.value = dots;
-    }else{
-        classlist.add("eye_close");
-        input.value = original;
-    }
-})
+   
